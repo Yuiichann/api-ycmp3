644446a7c69cdc14d5a2533a9e24b9a1b8d338c5
Yuii <yuiichan1357@gmail.com>
2022-10-13 04:26:03 +0700

    test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const db = require("./db");
 
 dotenv.config();
+
+db.connect();
 
 const PORT = process.env.PORT || 5000;
 
